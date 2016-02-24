c27e1a9e6bb68ff8f655cb314ad37343f33019a7
Mitchell Henke <mitchh23@gmail.com>
2016-02-24 10:32:21 -0600

    allow config via environment

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :link_shortener, base_url: "http://localhost:4000"
+config :link_shortener, base_url: System.get_env("BASE_URL") || "http://localhost:4000"
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
